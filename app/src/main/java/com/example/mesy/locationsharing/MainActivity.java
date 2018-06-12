de5da84b04bb6622934a9837112dd8a8261d441b
Mesy Macos <mercymarkus1796@gmail.com>
2018-06-12 20:37:46 +0100

    Added Floating Action Button,linked to MapsActivity

---
+++ app/src/main/java/com/example/mesy/locationsharing/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mesy.locationsharing;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+    }
+
+
+
+
+}
